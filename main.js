71f9aa2326fd66950ee89a5cce6b2846540d0a23
AnnaLenaO <annalenao@me.com>
2023-10-03 09:10:46 +0200

    VG-level

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// Fetch completed, input, button, message & list
+// Get completed, input, button, message & list
 const completed = document.querySelector("#allComplete")
 const input = document.querySelector("#inputToDo");
 const button = document.querySelector("#saveBtn");
@@ -27,7 +27,7 @@ button.addEventListener("click", function () {
     // Declare the variable text with value from input
     const text = input.value;
 
-    // Only text written from input can be added to the list
+    // No emty input can be added to the list
     if (text.length == 0) {
         message.innerText = "Write your task in the box";
         return;
@@ -50,25 +50,25 @@ button.addEventListener("click", function () {
     // Create a span element in the li element
     const toDoLabel = document.createElement("span");
 
-    // Add text to the span element & adding it to the li element
+    // Add text to the span element & add it to the li element
     toDoLabel.innerText = text;
     toDo.appendChild(toDoLabel);
 
     // Create a new span element (trashcan) in the li element
     const trashcan = document.createElement("span");
 
-    // Add traschcan symbol to the new span element & adding it to the li element
+    // Add traschcan symbol to the new span element & add it to the li element
     trashcan.innerHTML = " &#128465;";
     trashcan.setAttribute("class", "deletetoDo");
     toDo.appendChild(trashcan);
 
-    // Add EventLlistener to the span & uppdate countComplete
+    // Add EventLlistener to the first span & uppdate countComplete & status for toDo in toDoArray
     toDoLabel.addEventListener("click", function () {
 
         // Alternate between completed & not completed toDo
         if (toDo.getAttribute("class") == "markComplete") {
 
-            // Unmark the toDo as completed & decrease countComplete
+            // Unmark the toDo as completed & later decrease countComplete
             toDo.setAttribute("class", "");
 
             // Change status for a toDo in toDoArray to false
@@ -78,7 +78,7 @@ button.addEventListener("click", function () {
             countComplete--;
         }
         else {
-            // Mark the toDo as completed & increase countComplete
+            // Mark the toDo as completed & later increase countComplete
             toDo.setAttribute("class", "markComplete");
 
             // Change status for a toDo in toDoArray to true
@@ -92,7 +92,7 @@ button.addEventListener("click", function () {
         completed.innerText = `${countComplete} completed`;
     })
 
-    // Add EventListener to the trashcan for deleting a toDo
+    // Add EventListener to trashcan for deleting a toDo & uppdate countComplete & extraToDoArray
     trashcan.addEventListener("click", function () {
 
         // Uppdate countComplete
@@ -100,10 +100,11 @@ button.addEventListener("click", function () {
             countComplete--
         }
 
+        // Create visible text for number of completed toDo by using a string template
         completed.innerText = `${countComplete} completed`;
 
         // Uppdate extraToDoArray
-        //  Declare the span to declare the index so that text can be removed from extraToDoArray
+        // Create a new span element to declare the index so text can be removed from extraToDoArray
         let removeText = toDo.firstChild.firstChild.textContent;
         let removeIndex = extraToDoArray.indexOf(removeText);
         extraToDoArray.splice(removeIndex, 1);
